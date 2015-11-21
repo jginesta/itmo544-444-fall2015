7f35bf1ed96454a732b5bd80600587896b853fb6
jginesta <jginesta@hawk.iit.edu>
2015-11-21 10:13:16 -0600

    UPdating MP2

--- MP2.php
+++ MP2.php
@@ -2,18 +2,55 @@
 
 require 'vendor/autoload.php';
 
-use Aws\Rds\RdsClient;
+use Aws\Sns\SnsClient;
 
 $sns = SnsClient::factory(array(
 'version' => 'latest',
 'region' => 'us-east-1'
 ));
-$Arn = $sns->createTopic([
-'Name' => 'MP2-SNS-test',
+$ArnArray = $sns->createTopic([
+'Name' => 'mp2-jgl',
 ]);
 
+$Arn= $ArnArray['TopicArn'];
+echo "\r\n";
+echo "This is the Arn: $Arn";
 
-echo $Arn;
+
+$topicAttributes = $sns->getTopicAttributes(array(
+    'TopicArn' => "$Arn",
+    'AttributeName'=>'DisplayName',
+    'AttributeValue'=>'mp2-jgl',
+));
+echo "\r\n";
+echo "Subscriptions pending: {$topicAttributes['Attributes']['SubscriptionsPending']}";
+echo "\r\n";
+echo "Subscriptions confirmed: {$topicAttributes['Attributes']['SubscriptionsConfirmed']} ";
+
+$listSubscriptions = $sns->listSubscriptionsByTopic(array(
+    // TopicArn is required
+    'TopicArn' => "$Arn",
+));
+
+$endpointsubscriptions=$listSubscriptions['Subscriptions'][0]['Endpoint'];
+
+echo "\r\n";
+echo "The users subscribed to this topic are $endpointsubscriptions";
+
+if( in_array('jginesta@hawk.iit.edu', $endpointsubscriptions) ) {
+    echo "User already subscribe to topic";
+    }
+    else{
+    $subscribe = $sns->subscribe(array(
+    'TopicArn' => "$Arn",
+    'Protocol' => 'email',
+    'Endpoint' => 'jginesta@hawk.iit.edu',
+    ));
+    echo "\r\n";
+    echo "User subscribed with status {$subscribe['SubscriptionArn']}";
+
+    }
+
 
 
 
