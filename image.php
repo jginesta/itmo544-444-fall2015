64ce44a8e45f669db7a65a0b81b6c59c224963f3
jginesta <jginesta@hawk.iit.edu>
2015-11-23 16:39:56 -0600

    image upload

---
+++ image.php
@@ -0,0 +1,14 @@
+<?php
+
+header('Content-type: image/png');
+
+$images= new Imagick(glob('../images/*.png'));
+
+foreach($images as $image){
+	$image-> thumbnailImage(768,0);
+}
+
+
+$images-> writeImages('../images/out.png', false);
+echo $image;
+
