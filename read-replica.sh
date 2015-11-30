fd44d464e05edf32b744be7c6065c894a658b215
jginesta <jginesta@hawk.iit.edu>
2015-11-29 19:40:49 -0600

    final

--- read-replica.sh
+++
@@ -1,7 +0,0 @@
-#!bin/bash
-
-#Creating a read replica
-aws rds create-db-instance-read-replica --db-instance-identifier mp1-jgl-read --source-db-instance-identifier mp1-jgl --publicly-accessible
-sudo aws rds wait db-instance-available --db-instance-identifier mp1-jgl-read
-
-echo "read replica created"
